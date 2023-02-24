a1223a80431eb9ca7c11f227168ad48d9d48c014
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-02-24 09:59:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+ const textArea = document.getElementById('evaluatedText');
+ const wordCount = document.getElementById('wordCount');
+
+ textArea.addEventListener('input', () => {
+  const text = textArea.value;
+  const count = text.trim().split(/\s+/).length;
+     wordCount.textContent = count;
+ });
+   
